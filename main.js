29022f55ce7ee8f244aab6d482d55383c803bb7f
JuampiSoro <jpsoro@Juans-MacBook-Air.local>
2022-02-15 21:56:17 -0300

    js

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
+'use static';
+
 const opciones = document.querySelectorAll('.options');
+
+// const opcion = document.getElementsByClassName('options').focus();
 
 const btn = document.querySelector('.boton');
 
+const prueba = document.querySelector('img');
+
+const funcionPrueba = function (item) {
+    item.addEventListener("click", function () {
+        console.log("tetas")
+    })
+}
+
+
 opciones.forEach(opciones =>
-    opciones.addEventListener("click", () => btn.classList.remove("hide"))
+    opciones.addEventListener("click", () => btn.classList.add("see"))
 )
+
+
